6fe9ae5df2d639425929d86501174ae15dbc3b87
SreenaathV <sreenaathv8@gmail.com>
2026-01-11 20:52:10 +0530

    sw update

--- apps/web/dev-dist/sw.js
+++ apps/web/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ba5c9bb8'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.nqd0og85h6o"
+    "revision": "0.pe9aa2m7rkg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
