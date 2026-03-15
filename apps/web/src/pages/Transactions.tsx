ce4e652f52aa8ed192f183d5202fefaac52e7569
SreenaathV <sreenaathv8@gmail.com>
2026-03-15 21:13:15 +0530

    fix

--- apps/web/src/pages/Transactions.tsx
+++ apps/web/src/pages/Transactions.tsx
@@ -339,7 +339,7 @@ export default function Transactions() {
         columns={columns}
         dataSource={transactions}
         loading={isLoading}
-        rowKey="id"
+        rowKey="transactionId"
         rowSelection={{
           selectedRowKeys,
           getCheckboxProps: (record: any) => ({
